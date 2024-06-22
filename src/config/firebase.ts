955e12c340d8c8ad0b2de8f25251947de3750ff6
Ezzdin Atef <ezzdin1125@gmail.com>
2024-06-22 23:05:40 +0300

    chore: Update npm dependencies

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCoi83vPOxVV2i2IJ702TKtXudL2dL_GnU",
+  authDomain: "rtimesheet-73195.firebaseapp.com",
+  projectId: "rtimesheet-73195",
+  storageBucket: "rtimesheet-73195.appspot.com",
+  messagingSenderId: "746113993629",
+  appId: "1:746113993629:web:e7dfb423a565aee857fd74",
+  measurementId: "G-PF7PDNZLKZ",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
